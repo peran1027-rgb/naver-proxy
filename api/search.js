97c6b200da01ca62458467fdc8ce3757ae7508fe
peran1027-rgb <peran1027@gmail.com>
2026-06-19 17:04:48 +0900

    Create search.js

---
+++ api/search.js
@@ -0,0 +1,30 @@
+jsexport default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  const { query, start = 1 } = req.query;
+
+  if (!query) {
+    return res.status(400).json({ error: 'query is required' });
+  }
+
+  const CLIENT_ID = process.env.NAVER_CLIENT_ID;
+  const CLIENT_SECRET = process.env.NAVER_CLIENT_SECRET;
+
+  const url = `https://openapi.naver.com/v1/search/shop.json?query=${encodeURIComponent(query)}&display=100&start=${start}&sort=sim`;
+
+  const response = await fetch(url, {
+    headers: {
+      'X-Naver-Client-Id': CLIENT_ID,
+      'X-Naver-Client-Secret': CLIENT_SECRET,
+    },
+  });
+
+  const data = await response.json();
+  return res.status(200).json(data);
+}
