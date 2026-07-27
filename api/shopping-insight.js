e5dbb7a776235fbeea208f97f180258c2ed63d6b
peran1027-rgb <peran1027@gmail.com>
2026-07-27 17:17:41 +0900

    Update shopping-insight.js

--- api/shopping-insight.js
+++ api/shopping-insight.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
 
   try {
     const response = await fetch(
-      'https://naverapihub.apigw.ntruss.com/datalab/v1/shopping/categories/keywords',
+      'https://naverapihub.apigw.ntruss.com/naver-datalab-shopping/v1/categories/keywords',
       {
         method: 'POST',
         headers: {
